f0534172fdec568a3974076e1d1189d8fd8572e5
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-16 23:09:33 +0000

    created tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,24 @@ CREATE DATABASE workforce_db;
 USE workforce_db;
 
 CREATE TABLE departments (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
