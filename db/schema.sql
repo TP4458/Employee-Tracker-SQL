b4bcddad8b2fd85374c36f755399270b17287a5a
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-16 22:42:44 +0000

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS workforce_db;
+CREATE DATABASE workforce_db;
+
+USE workforce_db;
+
+CREATE TABLE departments (
+
+);
+
+CREATE TABLE roles (
+
+);
+
+CREATE TABLE employees (
+
+);
