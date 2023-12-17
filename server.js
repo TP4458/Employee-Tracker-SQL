a6e17a3f70627cb7c962108258aa5c449800fba4
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-17 02:40:34 +0000

    added main question prompt

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { default: inquirer } = require("inquirer");
 const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
@@ -16,3 +17,46 @@ const db = mysql.createConnection(
     }, 
     console.log("connected to workforce_db database.")
 )
+
+const questions = async () => {
+    const answer = await inquirer.prompt([{
+        type: "list", 
+        name: "options",
+        message: "select your option:",
+        choices: [
+            "View all departments",
+            "View all employee roles",
+            "View all employees",
+            "Add a new department",
+            "Add a new role",
+            "Add a new employee",
+            "Update an employee role"
+        ],
+    }])
+}
+
+
+
+const viewDepts = () => {
+
+}
+
+const viewRoles = () => {
+    
+}
+
+const viewEmpl = () => {
+    
+}
+
+const addDept = () => {
+    
+}
+
+const addRole = () => {
+    
+}
+
+const addEmpl = () => {
+    
+}
